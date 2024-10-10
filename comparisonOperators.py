6424947dfb97ef371a48b094cebc0ffebc9654a2
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-10-10 09:36:28 -0400

    final submission for python operators

---
+++ comparisonOperators.py
@@ -0,0 +1,8 @@
+a = 32
+b = 6
+print('Two numbers are equal or not:', a == b)
+print('Two numbers are not equal or not:', a != b)
+print('a is less than or equal to b:', a <= b)
+print('a is greater than or equal to b:', a >= b)
+print('a is greater than b:', a > b)
+print('a is less than b:', a < b)
---
+++ membershipsOperators.py
@@ -0,0 +1,15 @@
+x = ["Rose", "Lotus"]
+print('Is value Present?', "Rose" in x)
+print('Is value not Present?', "Riya" not in x)
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+#check if 'H' is present in x string
+print('H' in x) # print True
+#check if 'hello' is present in x string
+print('hello' not in x) # print False
+#check if 1 key is present in y 
+print(1 in y) # print True
+#check if 'a' key is present in y
+print('a' in y) # print False
